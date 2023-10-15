01a0062aa98a911b2b6d86bc4f63db35a69aa4be
Esonhugh <esonhughoutside@gmail.com>
2023-10-15 16:34:41 +0800

    update contents

---
+++ readme-update.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+export MODULE_LIST=`tree . -f`
+export STANDARD_MODULE_STRUCT=`cat ./metasploit-modules-folder-structure.md`
+
+cat <<EOF > README.md
+# Self Metasploit
+
+Esonhugh self maintained Metasploit modules folder
+
+## Usage
+
+\`\`\`bash
+git clone https://github.com/Esonhugh/Self-Metasploit.git
+echo "loadpath \${PWD}/Self-Metasploit" >> $HOME/.msf4/msfconsole.rc
+\`\`\`
+
+> put into command loadpath \$PWD/Self-Metasploit into $HOME/.msf4/msfconsole.rc file
+> 
+> That will make metasploit auto loading modules from Self-Metasploit folder when msfconsole start without -r options
+>
+
+## Modules List
+
+\`\`\`
+${MODULE_LIST}
+\`\`\`
+
+## Standard Modules folder list in Metasploit
+
+\`\`\`
+${STANDARD_MODULE_STRUCT}
+\`\`\`
+
+EOF
